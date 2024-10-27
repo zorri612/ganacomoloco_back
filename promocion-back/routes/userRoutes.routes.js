81516e861740149b7d931f731556b05dcda0d2ca
Nicolas Zc <zorrillanico200312@gmail.com>
2024-10-26 22:28:23 -0500

    Delete promocion-back directory

--- promocion-back/routes/userRoutes.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {validateCredentials, Signup} = require('./controllers/users.js');
-
-router.post('/login', validateCredentials);
-router.post('/signup', Signup);
-
-module.exports = router;
