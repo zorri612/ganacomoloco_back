30eb85f5c70ae8c624e34e23b6bb04176631521e
Nicolas <zorrillanico200312@gmail.com>
2024-10-26 22:35:57 -0500

    subí back parcial

---
+++ routes/userRoutes.routes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const {validateCredentials, Signup} = require('./controllers/users.js');
+
+router.post('/login', validateCredentials);
+router.post('/signup', Signup);
+
+module.exports = router;
